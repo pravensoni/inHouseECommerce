2d9633f53d51374994f61c5e06ac3927b3a7087e
praveen soni <praveensoni1992@gmail.com>
2017-07-02 22:29:53 +0530

    adding mysql connection and get product details api

--- src/main/java/com/praveen/ecommerce/WebController.java
+++ src/main/java/com/praveen/ecommerce/WebController.java
@@ -1,13 +1,21 @@
 package com.praveen.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.praveen.ecommerce.dao.CommonDao;
+import com.praveen.ecommerce.models.Product;
 
 @RestController
 public class WebController {
 
-	@RequestMapping("/test")
-	public Test test() {
-		return new Test(1, "content");
+	@Autowired
+	CommonDao commonDao;
+
+	@RequestMapping("/product/{productId}")
+	public Product test(@PathVariable("productId") int productId) {
+		return commonDao.getProduct(productId);
 	}
 }
--- src/main/java/com/praveen/ecommerce/dao/CommonDao.java
+++ src/main/java/com/praveen/ecommerce/dao/CommonDao.java
@@ -1,5 +1,49 @@
 package com.praveen.ecommerce.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.praveen.ecommerce.models.Product;
+
+@Component
 public class CommonDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public Product getProduct(int productId) {
+		try {
+			return jdbcTemplate.queryForObject("SELECT * FROM product_detail where id=?", new Object[] { productId },
+					getProductRM());
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private RowMapper<Product> getProductRM() {
+		return new RowMapper<Product>() {
+			@Override
+			public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Product product = new Product();
+				product.setDescription(rs.getString("description"));
+				product.setId(rs.getInt("id"));
+				// product.setImageLinks(List<String>);
+				product.setOrigPrice(rs.getDouble("orig_price"));
+				product.setPrice(rs.getDouble("price"));
+				product.setTitle(rs.getString("title"));
+				// product.setVariants(List<Variant>);
+				return product;
+			}
+		};
+	}
 
 }
